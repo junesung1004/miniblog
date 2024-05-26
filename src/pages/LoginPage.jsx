72b0a3f03338083ff02ad0d1a1c6bca0b5c2fd14
junesung1004 <junecoding1004@gmail.com>
2024-05-26 22:02:56 +0900

    feat : mini blog semi project router & redux 초기 셋팅 설정 완료

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import "./LoginPage.css";
+
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <>
+      <div className="login-container">
+        <h1>로그인페이지입니다.</h1>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SignUpPage.css";
+
+export default function SignUpPage() {
+  return (
+    <>
+      <div className="sign-up-container">
+        <h1>회원가입 페이지입니다.</h1>
+      </div>
+    </>
+  );
+}
